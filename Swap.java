ce6db6ab24db5aab7f65242390a12e8369ca3ded
SusmithaPerikala <98683074+SusmithaPerikala@users.noreply.github.com>
2024-03-14 12:57:43 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Swap {
+    
+    public static void main(String[] args) {
+        int a = 200;
+        int b = 300;
+
+        System.out.println("Before swapping :");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println();
+        System.out.println("After swapping :");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
